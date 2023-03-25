c583bb89c2a27b221f4b5fd8b48af8b6dcdadf06
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2023-03-25 11:07:08 +0300

    made changes

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "https://shopyetuapi.onrender.com/api"});
+const instance = axios.create({
+  baseURL: "https://shopyetuapi.onrender.com/api",
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
 // const instance = axios.create({ baseURL: "http://localhost:4000/api" });
 // export const axiosPrivate = axios.create({
 //   baseURL: "http://localhost:4000/api",
@@ -9,8 +13,8 @@ const instance = axios.create({ baseURL: "https://shopyetuapi.onrender.com/api"}
 
 export const axiosPrivate = axios.create({
   baseURL: "https://shopyetuapi.onrender.com/api",
+  headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
-
 
 export default instance;
